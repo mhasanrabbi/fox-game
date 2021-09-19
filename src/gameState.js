d3c0e9d8d015f8d827c8c6616b051742e044d4cb
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-09-19 21:49:06 +0600

    adding new states :rotating_light:

--- src/gameState.js
+++ src/gameState.js
@@ -1,14 +1,55 @@
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1,
   tick() {
     this.clock++;
-    console.log(`clock ${this.colck}`);
+    console.log(this.clock);
     return this.clock;
   },
+  startGame() {
+    console.log("hatching");
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+  },
+  wake() {
+    console.log("awaken");
+    this.current = "IDLING";
+    this.wakeTime = -1;
+  },
   handleUserAction(icon) {
-    console.log(icon);
+    if (
+      ["SlEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)
+    ) {
+      return;
+    }
+    if (this.current === "INIT" || this.current === "DEAD") {
+      this.startGame();
+      return;
+    }
+    switch (icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanUpPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+    }
+  },
+  changeWeather() {
+    console.log("changeWeather");
+  },
+  cleanUpPoop() {
+    console.log("cleanUpPoop");
+  },
+  feed() {
+    console.log("feed");
   },
 };
+window.gameState = gameState;
 
+export const handleUserAction = gameState.handleUserAction.bind(gameState);
 export default gameState;
